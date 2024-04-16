470973dbc55199844d4e4d4d5162cba119beeb13
Petr Kratochvíl <1684874+petr-kratochvil@users.noreply.github.com>
2024-04-16 16:44:09 +0200

    Add mui, emotion; Add menu

--- src/pages/RootPage.js
+++ src/pages/RootPage.js
@@ -1,22 +1,45 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 import "../App.css";
 
 export default function RootPage() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <div className="App-layout">
         <div style={{ display: "flex", flexDirection: "column" }}>
           <header className="App App-header">
-            <p>Astro Transits</p>
+            <div>Astro Transits</div>
+            <div onClick={handleClick} style={{position: 'absolute', right: '10px', paddingRight: '10px', cursor: 'pointer'}}>☰</div>
           </header>
-          <div className="App-bar">
-            {/* <Link className="Router-link" to={"/"}>
-              Transits
-            </Link> */}
-          </div>
         </div>
+        <Menu
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          autoFocus={false}
+          disableScrollLock={true}
+        >
+          <MenuItem onClick={handleClose}>
+            <Link className="Router-link" to={"/"}>
+              Transits
+            </Link></MenuItem>
+          <MenuItem onClick={handleClose}>
+            <Link className="Router-link" to={"/tables"}>
+              Tables
+            </Link>
+          </MenuItem>
+        </Menu>
         <Outlet />
       </div>
     </>

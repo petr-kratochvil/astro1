1952601d94b18ad2c254a686b6523a957dcedc6c
Petr Kratochvíl <1684874+petr-kratochvil@users.noreply.github.com>
2024-10-05 17:54:01 +0200

    use dropdown in transits page

--- src/pages/TransitsPage.js
+++ src/pages/TransitsPage.js
@@ -6,15 +6,29 @@ import { getBaseDateJson, getSavedData } from "../utils/localStorage";
 
 export default function TransitsPage() {
   useTitle();
+
+  const savedDataList = getSavedData();
+  const [selectedBaseDate, setSelectedBaseDate] = React.useState(
+    savedDataList.length > 0 ? 0 : null
+  );
+  const options = savedDataList.map((item, index) => ({
+    value: index,
+    label: item.name,
+  }));
+
   const [data, setData] = React.useState([]);
-  const baseDatePetr = { year: 1988, month: 7, day: 12, hour: 2 };
-  const baseDateJitka = { year: 1973, month: 3, day: 26, hour: 2.25 };
-  const [baseDateJson, setBaseDateJson] = React.useState(baseDatePetr);
-  const [person, setPerson] = React.useState("Petr");
+  const [baseDateJson, setBaseDateJson] = React.useState(
+    savedDataList.length > 0 ? savedDataList[0] : null
+  );
+  const [person, setPerson] = React.useState(
+    savedDataList.length > 0 ? savedDataList[0].name : null
+  );
   const [transitDate, setTransitDate] = React.useState(new Date());
 
   function callGetTransits(baseDate, transitDate) {
-    getTransits(baseDate, transitDate).then(data => setData(data));
+    if (baseDate && transitDate) {
+      getTransits(baseDate, transitDate).then((data) => setData(data));
+    }
   }
 
   function addDays(date, days) {
@@ -55,35 +69,39 @@ export default function TransitsPage() {
     };
   });
 
-  const savedDataList = getSavedData();
-
-  const buttonsMenuStyle = { display: "flex", flexDirection: "row", alignItems: "center", flexWrap: "wrap", marginBottom: "10px" }
+  const buttonsMenuStyle = {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+    marginBottom: "10px",
+  };
 
   return (
     <div>
-      <h1 style={{color: 'slateblue', textAlign: 'center'}}>{person}'s transits</h1>
-      <div style={buttonsMenuStyle}>
-        <button
-          onClick={() => setBaseDateJson(baseDatePetr) + setPerson("Petr")}
+      <h1 style={{ color: "slateblue", textAlign: "center" }}>
+        Tranzity
+      </h1>
+      <div style={{margin: '10px'}}>
+        Vyberte záznam: &nbsp;
+        <select
+          style={{minWidth: '150px', minHeight: '30px'}}
+          value={selectedBaseDate}
+          onChange={(e) => {
+            setSelectedBaseDate(e.target.value);
+            setBaseDateJson(savedDataList[e.target.value]);
+            setPerson(savedDataList[e.target.value].name);
+            console.log('tady tady 1')
+          }}
         >
-          Petr
-        </button>
-        <button
-          onClick={() => setBaseDateJson(baseDateJitka) + setPerson("Jitka")}
-        >
-          Jitka
-        </button>
-        {savedDataList.map((item, index) => item &&(
-          <button
-            key={index}
-            onClick={() => setBaseDateJson(getBaseDateJson(index)) + setPerson(item.name || 'Saved date '+index)}
-          >
-            {item.name || 'Saved date '+index}
-          </button>
-        ))}
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
       </div>
       <div style={buttonsMenuStyle}>
-        
         <button onClick={() => setTransitDate(addDays(transitDate, -1))}>
           &nbsp;&nbsp;&lt;&lt;&nbsp;&nbsp;
         </button>
